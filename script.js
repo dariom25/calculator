d9f6f4585fb5177edd53859142b5ca092097e65a
dariom25 <dario@maciey.de>
2023-07-29 23:00:08 +0200

    Create buttons and add them to the DOM

---
+++ script.js
@@ -0,0 +1,21 @@
+const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]
+const buttonContainer = document.querySelector(".button-container")
+
+
+
+
+
+
+
+
+
+
+
+
+//create buttons and add them to the DOM
+numbers.forEach((number) => {
+    let numberBtn = document.createElement("button");
+    numberBtn.setAttribute("name", number);
+    numberBtn.classList.add("buttons");
+    buttonContainer.appendChild(numberBtn);
+})
