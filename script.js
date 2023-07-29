53204c58b2a2a612661c8fae30012f59b4b8175a
dariom25 <dario@maciey.de>
2023-07-29 23:28:33 +0200

    Create const for all buttonRows

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const calculatorButtons = [["C", "/", "*"], ["7", "8", "9", "-"], ["4", "5", "6", "+"], ["1", "2", "3"], ["0"]]
 const buttonContainer = document.querySelector(".button-container")
+const buttonRows = document.querySelectorAll(".row")
 
 
 
@@ -10,14 +11,14 @@ const buttonContainer = document.querySelector(".button-container")
 
 
 //create 5 divs to contain a row of buttons
-const buttonRows = () => {
+const createButtonRows = () => {
     for (let i = 0; i < 5; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
         buttonContainer.appendChild(row);
     }
 }
-
+createButtonRows()
 
 //create buttons and add them to the DOM
 calculatorButtons.forEach((button) => {
