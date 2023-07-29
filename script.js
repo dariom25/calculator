3cee6e33851c85c5888c0c2fcfee30b56e416f7d
dariom25 <dario@maciey.de>
2023-07-29 23:20:36 +0200

    Edit the number array and number-loop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]
+const calculatorButtons = [["C", "/", "*"], ["7", "8", "9", "-"], ["4", "5", "6", "+"], ["1", "2", "3"], ["0"]]
 const buttonContainer = document.querySelector(".button-container")
 
 
@@ -9,13 +9,15 @@ const buttonContainer = document.querySelector(".button-container")
 
 
 
-
+//create 5 divs to contain a row of buttons
 
 
 //create buttons and add them to the DOM
-numbers.forEach((number) => {
-    let numberBtn = document.createElement("button");
-    numberBtn.textContent = number;
-    numberBtn.classList.add("buttons");
-    buttonContainer.appendChild(numberBtn);
+calculatorButtons.forEach((button) => {
+    button.forEach((element) => {
+        let btn = document.createElement("button");
+        btn.textContent = element;
+        btn.classList.add("buttons");
+        buttonContainer.appendChild(btn);
+    });
 })
