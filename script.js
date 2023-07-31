855a0d8a6f6469edfebff48ba20c5b843070aa32
dariom25 <dario@maciey.de>
2023-07-31 22:58:15 +0200

    Add todo-comments

--- script.js
+++ script.js
@@ -13,10 +13,20 @@ function addEventListenerToButtons(nodelist) {
 //display buttons in display-field
 const displayButton = function(node) {
     const btnText = node.innerText;
-    const displayText = document.createElement("div") //maybe it is smarter to add the text to the div than to create new divs
+    const displayText = document.createElement("div") //maybe it is smarter to add the text to the div than to create new divs --> how to deal with linebreak?
     displayText.classList.add("calculations")
     displayText.textContent = btnText
     display.appendChild(displayText)
 }
 
 addEventListenerToButtons(allBtns)
+
+//TODO:
+//C-Button: Delete text in display
+//+ Button: Function to add numbers
+//- Button: Function to subtract numbers
+//* Button: Function to multiply numbers
+// /-Button: Funcion to divide numbers
+//Logic like Punkt vor Strich
+//= Button to calculate the result
+//Calculation-logic: Add every event into an array?
