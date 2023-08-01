6b093e77212dd2a0944cea2c38c438e4ad795383
dariom25 <dario@maciey.de>
2023-08-01 19:11:27 +0200

    Change structure of addEventListenersToButtons

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ const display = document.querySelector(".display")
 
 //add eventlisteners to buttons
 function addEventListenerToButtons(nodelist) {
-    nodelist.forEach(node => {
+    nodelist.forEach((node, index) => {
         node.addEventListener("click", function() {
-            displayButton(node);
+            if (index === 0) {
+                displayButton(node);
+            } else if (index === 1) {
+                displayButton(node);
+            } else if (index === 2) {
+                displayButton(node);
+            } else if (index === 14) {
+                displayButton(node);
+            } else if (index === 15) {
+                displayButton(node);
+            } else if (index === 16) {
+                displayButton(node);
+            } else {
+                displayButton(node);
+            }
+            
         });
     });
 }
